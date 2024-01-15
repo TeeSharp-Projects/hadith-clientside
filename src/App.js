130d7cfcc00c71f8f97cfa3ab17812f08d6b3325
shabitee <94993051+shabitee@users.noreply.github.com>
2024-01-15 13:18:41 +0000

    feat:imported Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
 
 
 
@@ -6,6 +7,9 @@ function App() {
   return ( 
   <>
       <BrowserRouter>
+      <Header>
+
+      </Header>
 
       </BrowserRouter>
   
