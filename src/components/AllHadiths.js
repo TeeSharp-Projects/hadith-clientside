9878f90c937ae51a801f10bc1bae3059b5516d7f
shabitee <94993051+shabitee@users.noreply.github.com>
2024-01-22 12:15:33 +0000

    rendered

--- src/components/AllHadiths.js
+++ src/components/AllHadiths.js
@@ -5,12 +5,13 @@ import axios from "axios";
 function AllHadith(){
 
     const [results, setresults] = useState([]);
+    const [showItems,setshowItems] = useState(false);
     useEffect(()=>{
         const getAllHadith = async () => {
             let allHadithApi = await axios.get(`${process.env.REACT_APP_SERVER}/hadithapi`);
             // console.log('inside useEffect AllHadiths', allHadithApi.data);
             setresults(allHadithApi.data);
-            setshowitems(true);
+            setshowItems(true);
 
         };
         getAllHadith();
