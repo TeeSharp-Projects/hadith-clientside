d41a3f2faa1508b284ddf273d6bf76e33154c6c5
shabitee <94993051+shabitee@users.noreply.github.com>
2024-01-22 12:27:45 +0000

    added card in the item.js

---
+++ src/components/Item.js
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function item(){
+    return(
+        <Card style={{ width: "34rem",margin:"20px" }}>
+      <Card.Img style ={{height:"200px",}}variant="top" src="https://tse2.mm.bing.net/th?id=OIP.YXo5f7XYkYBGi5jCEVuRHQHaLJ&pid=Api&P=0" />
+      <Card.Body>
+      <Card.Title>Book Name: {props.item.book.bookName}</Card.Title>
+      <Card.Title>Chapter: {props.item.chapter.chapterEnglish}</Card.Title>
+      <Card.Title>Narrator: {props.item.englishNarrator}</Card.Title>
+        <Card.Text className = "hadithScroll">{props.item.hadithEnglish}</Card.Text>
+        <Button  className="buttons"variant="primary" onClick={()=>{addToFav(props.item)}}>Add to favourite</Button>
+      </Card.Body>
+    </Card>
+    );
+}
+
+export default Item;
